493ca9e75d984f0fe49f2f4461cb25ddf5fec554
Sanubinha <sanuncieldamacenocbranco@gmail.com>
2024-11-24 11:08:25 -0300

    main page

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,11 +6,11 @@
 
         <title>@yield('titulo')</title>
 
-        <!-- Fonts -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/styles.css">
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="/css/index.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet"> 
 
     </head>
     <body>
@@ -24,7 +24,7 @@
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#"><strong>Home</strong></a>
+                    <a class="nav-link active" aria-current="page" href="#"><strong>Jogos</strong></a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#"><strong>Link</strong></a>
@@ -41,10 +41,6 @@
                     </ul>
                   </li>
                 </ul>
-                <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
               </div>
             </div>
         </nav>
@@ -55,7 +51,7 @@
         </div>
         <footer>
             <div id="texto_footer">
-              <p>ProjetoLaravel &copy; 2024</p>
+              <p>GameForge &copy; 2024</p>
               <p>Desenvolvido por Sanunciel Damaceno</p>
             </div>
         </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,64 @@
 @extends('layouts.index')
-@section('titulo', 'ProjetoTeste')
+@section('titulo', 'GameForge')
 @section('content')
+
+<link rel="stylesheet" href="/css/welcome.css">
+
+<div id="banner_principal">
+      <img src="img/banner.png" class="d-block w-100" alt="imagem de um console" width="500px" height="500px">
+</div>
+
+<h2>JOGOS MAIS VENDIDOS</h2>
+
+<div id="jogos">
+  <div class="card" style="width: 18rem;">
+    <img src="img/jogo1.png" class="card-img-top" width="100px" height="300px" alt="jogo um">
+    <div class="card-body">
+      <h5 class="card-title">The Loocker</h5>
+      <p class="card-text">The Loocker é um jogo de aventura e enigmas em um mundo vibrante, onde tudo está de olho em você! Explore cenários coloridos, desvende segredos e encare desafios surpreendentes para revelar o mistério por trás dos olhos curiosos.</p>
+      <a href="#" class="btn btn-secondary">Comprar</a>
+
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="img/jogo2.PNG" class="card-img-top" width="100px" height="300px" alt="jogo dois">
+    <div class="card-body">
+      <h5 class="card-title">Gold Runner</h5>
+      <p class="card-text">Gold Runner é um jogo de ação onde você explora minas perigosas em busca de ouro, enfrentando inimigos, armadilhas e desafios. Colete tesouros, resolva enigmas e corra contra o tempo em uma aventura cheia de adrenalina e aventura. </p>
+      <a href="#" class="btn btn-secondary">Comprar</a>
+
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="img/jogo3.png" class="card-img-top" width="100px" height="300px" alt="jogo três">
+    <div class="card-body">
+      <h5 class="card-title">BigBall</h5>
+      <p class="card-text">BigBall é um jogo arcade divertido e desafiador onde você controla uma bola gigante através de obstáculos, plataformas e terrenos variados. Use habilidade e estratégia para evitar armadilhas e coletar bônus pelo caminho.</p>
+      <a href="#" class="btn btn-secondary">Comprar</a>
+
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="img/jogo4.jpg" class="card-img-top" width="100px" height="300px" alt="jogo três">
+    <div class="card-body">
+      <h5 class="card-title">Life Simulator</h5>
+      <p class="card-text">Life Simulator é um jogo de simulação onde você cria e gerencia a vida de um personagem, tomando decisões sobre carreira, relacionamentos, saúde e sonhos, moldando seu caminho e sua personalidade a cada clique..</p>
+      <a href="#" class="btn btn-secondary">Comprar</a>
+
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="img/jogo5.jpg" class="card-img-top" width="100px" height="300px" alt="jogo três">
+    <div class="card-body">
+      <h5 class="card-title">Call of Darkness</h5>
+      <p class="card-text">Em meio a florestas envoltas em névoa, ruínas de civilizações antigas e lugares abandonados, as sombras guardam histórias esquecidas, e nem todos os que houvem o chamado conseguem sair vivos para contá-las.</p>
+      <a href="#" class="btn btn-secondary">Comprar</a>
+
+    </div>
+  </div>
+</div>
 @endsection
