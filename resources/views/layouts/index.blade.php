93a9fe106151562e461da117bf702437d725f4cf
Sanubinha <sanuncieldamacenocbranco@gmail.com>
2025-02-16 11:22:01 -0300

    view pra cadastrar usuario

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -17,7 +17,7 @@
       <header>
         <nav class="navbar navbar-expand-lg bg-secondary">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img src="/img/logo.png" width=50px heigth=50px alt=""></a>
+              <a class="navbar-brand" href="/"><img src="/img/logo.png" width=50px heigth=50px alt=""></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -27,7 +27,7 @@
                     <a class="nav-link active" aria-current="page" href="#"><strong>Jogos</strong></a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#"><strong>Link</strong></a>
+                    <a class="nav-link" href="/cadastroUser"><strong>Cadastre-se</strong></a>
                   </li>
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cadastroUser', function () {
+    return view('usuario.cadastrar');
+});
